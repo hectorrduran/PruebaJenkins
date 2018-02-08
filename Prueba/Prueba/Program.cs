6ac8b7ca1d738de2c6104a91e9b16410f21b47c9
hector duran <hector@adaptativamente.cl>
2018-02-08 09:52:47 -0400

    Primer Subida

---
+++ Prueba/Prueba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(texto());
+        }
+        public static string texto()
+        {
+            return "Hola mundo";
+        }
+    }
+}
---
+++ Prueba/PruebaTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prueba;
+
+namespace PruebaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hola mundo", Program.texto());
+        }
+    }
+}
